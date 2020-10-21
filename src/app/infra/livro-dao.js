a5e740b915a168517dc95c6cab36cd38346b092a
Lucas Rodrigues Abreu <lucasrodriguesabreu@hotmail.com>
2020-10-21 01:40:20 -0300

    Implementado exclusão de livros

--- src/app/infra/livro-dao.js
+++ src/app/infra/livro-dao.js
@@ -7,11 +7,11 @@ class LivroDao {
     adiciona(livro) {
         return new Promise((resolve, reject) => {
             this._db.run(`
-                INSERT INTO LIVROS (
-                        titulo,
-                        preco,
-                        descricao
-                    ) values (?, ?, ?)
+                INSERT INTO livros (
+                    titulo, 
+                    preco,
+                    descricao
+                ) values (?,?,?)
                 `,
                 [
                     livro.titulo,
@@ -23,12 +23,12 @@ class LivroDao {
                         console.log(err);
                         return reject('Não foi possível adicionar o livro!');
                     }
-    
+
                     resolve();
-                } 
-            )       
+                }
+            )
         });
-    } 
+    }
 
     lista() {
         return new Promise((resolve, reject) => {
@@ -40,6 +40,71 @@ class LivroDao {
                     return resolve(resultados);
                 }
             )
+        });
+    }
+
+    buscaPorId(id) {
+
+        return new Promise((resolve, reject) => {
+            this._db.get(
+                `
+                    SELECT *
+                    FROM livros
+                    WHERE id = ?
+                `,
+                [id],
+                (erro, livro) => {
+                    if (erro) {
+                        return reject('Não foi possível encontrar o livro!');
+                    }
+                    return resolve(livro);
+                }
+            );
+        });
+    }
+
+    atualiza(livro) {
+        return new Promise((resolve, reject) => {
+            this._db.run(`
+                UPDATE livros SET
+                titulo = ?,
+                preco = ?,
+                descricao = ?
+                WHERE id = ?
+            `,
+            [
+                livro.titulo,
+                livro.preco,
+                livro.descricao,
+                livro.id
+            ],
+            erro => {
+                if (erro) {
+                    return reject('Não foi possível atualizar o livro!');
+                }
+
+                resolve();
+            });
+        });
+    }
+
+    remove(id) {
+
+        return new Promise((resolve, reject) => {
+            this._db.get(
+                `
+                    DELETE 
+                    FROM livros
+                    WHERE id = ?
+                `,
+                [id],
+                (erro) => {
+                    if (erro) {
+                        return reject('Não foi possível remover o livro!');
+                    }
+                    return resolve();
+                }
+            );
         });
     }
 }
